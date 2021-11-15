30fa30277d7c23efc46145c112937dae6535f6b3
prasanarebase <prasana.shinde.rebase@gmail.com>
2021-11-15 15:08:44 +0530

    Migrating changes

--- pyscripts/pivotCsv.py
+++ pyscripts/pivotCsv.py
@@ -1,12 +1,13 @@
 import sys;
 import os;
 from hestia_earth.utils.table import pivot_csv
+
 if not os.path.exists(sys.argv[3]):
     os.makedirs(sys.argv[3]);
-try:
-    df = pivot_csv(sys.argv[1]);
-    df.to_csv(sys.argv[2], index=None);
-    print('Converted successfully');
-except:
-    print('Error occured while converting');
+
+df = pivot_csv(sys.argv[1]);
+df.to_csv(sys.argv[2], index=None);
+csv = df.to_csv();
+print(csv);
+
 sys.stdout.flush();
--- src/test/convert/convert.test.ts
+++ src/test/convert/convert.test.ts
@@ -2,18 +2,33 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import supertest from 'supertest';
+import { spawn } from 'child_process';
+import * as fs from 'fs';
+import path from 'path';
 import app from '../..';
 
 describe('POST convert file', () => {
   it('should return error message', async () => {
     const resp = await supertest(app).post('/convert/file/');
     expect(resp.statusCode).equals(400);
-    expect(resp.body.data).to.equals('File not found');
+    expect(resp.body.error).to.equals('File not found');
   });
 
   it('should return success message', async () => {
     const resp = await supertest(app).post('/convert/file/').attach('file', 'mockdata/sample.csv');
     expect(resp.statusCode).equals(200);
-    expect(resp.body.data).to.equals('Converted successfully\n');
+    expect(resp.type).equals('text/csv');
+  });
+});
+
+describe('PYTHON script to convert file', () => {
+  it('should return success message', (done) => {
+    const pyresp = spawn('python', ['pyscripts/pivotCsv.py', path.resolve('mockdata/sample.csv'), path.resolve('mockdata/converted/sample.csv'), path.resolve('mockdata/converted/')]);
+    pyresp.stdout.on('data', () => {
+      const generatedOutput = fs.readFileSync(path.resolve('mockdata/converted/sample.csv'));
+      const output = fs.readFileSync(path.resolve('mockdata/output.csv'));
+      expect(generatedOutput.toString()).equals(output.toString());
+      done();
+    });
   });
 });
